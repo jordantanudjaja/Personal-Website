d72ef51f3e47098e306bf5dc19a21cf376ef597c
jordantanudjaja <jmt2218@columbia.edu>
2022-01-28 20:51:19 -0500

    Updated HTTPS in settings

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -5,7 +5,7 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY_PROD')
 DEBUG = False
-ALLOWED_HOSTS = ['www.jordantanudjaja.com', 'jordantanudjaja.com', 'jordantanudjaja.herokuapp.com', 'www.jordantanudjaja.herokuapp.com']
+ALLOWED_HOSTS = ['www.jordantanudjaja.com', 'jordantanudjaja.com']
 
 # AWS S3 parameters
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -14,12 +14,12 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = True # When files of the same name are uploaded, this will overwrite the previous file saved (I think this is better for me). so I have to be careful when uploading similar files if I don't want the previous one to be overwritten
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# # HTTPS settings
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# #
-# # # HSTS settings
-# SECURE_HSTS_SECONDS = 31536000 # 1 year
-# SECURE_HSTS_PRELOAD = True
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# HTTPS settings
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# HSTS settings
+SECURE_HSTS_SECONDS = 31536000 # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
